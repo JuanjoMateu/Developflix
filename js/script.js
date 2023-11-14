b250f6633d9582674c2dd4c05203dc519a41e362
JuanjoMateu <juanjo_mateu@hotmail.es>
2023-11-14 23:31:57 +0100

    Ejercicio terminado con una alternativa a ForEach

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 import peliculas from './peliculas.js'
+//genre_ids: genero
+//poster_path: imagenes
+//title: título
 
+const mostrarPeliculaPorGenero = (generoId, contenedorId) => {
+    const contenedor = document.getElementById(contenedorId)
+    const peliculaGenero = peliculas.filter((pelicula) => pelicula.genre_ids.includes(generoId))
+
+    let i = 0
+    while ( i < peliculaGenero.length) {
+    let pelicula = peliculaGenero[i]; 
+    let templatePelis = `
+    <div class="pelicula">
+        <img src="https://image.tmdb.org/t/p/w200/${pelicula.poster_path}" alt="${pelicula.title}" />
+        <h3>${pelicula.title}</h3>
+    </div>`
+    contenedor.innerHTML += templatePelis;
+    i++;
+}}
+
+mostrarPeliculaPorGenero(28, "genero-28")
+mostrarPeliculaPorGenero(53, "genero-53")
+mostrarPeliculaPorGenero(12, "genero-12")
